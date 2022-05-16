8af812bcad41d5b67d31d6a1151a9a349b7a5b8c
Kiseloff <30468578+Kiseloff@users.noreply.github.com>
2022-05-16 19:58:24 +0300

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,25 @@
+def getSrc() {
+    echo "getting the sources..."
+    sh 'rm -rf *'
+    sh 'git clone https://gitlab.com/nanuchi/java-maven-app.git .'
+}
+
+def buildJar() {
+    echo "building the app..."
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t 192.168.88.14:8083/java-maven-app:3.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin 192.168.88.14:8083"
+        sh 'docker push 192.168.88.14:8083/java-maven-app:3.0'
+    }
+}
+
+def deployApp() {
+    echo "deploying the app..."
+}
+
+return this
