adbad521c4d757e9f649cca3bc5070787fca2976
Kiseloff <30468578+Kiseloff@users.noreply.github.com>
2022-05-17 10:57:18 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,20 @@
 def getSrc() {
     echo "getting the sources..."
-    sh "rm -rf $PROJECT_PATH"
-    sh "git clone -b jenkins-jobs https://gitlab.com/nanuchi/java-maven-app.git $PROJECT_PATH"
+    echo "${PROJECT_PATH}"
+    sh "rm -rf ${PROJECT_PATH}"
+    sh "git clone -b jenkins-jobs https://gitlab.com/nanuchi/java-maven-app.git ${PROJECT_PATH}"
 }
 
 def buildJar() {
     echo "building the app..."
-    sh "mvn package -f $PROJECT_PATH"
+    sh "mvn package -f ${PROJECT_PATH}"
 }
 
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'ls -la'
-        sh "docker build -t 192.168.88.14:8083/java-maven-app:4.0 $PROJECT_PATH"
+        sh "docker build -t 192.168.88.14:8083/java-maven-app:4.0 ${PROJECT_PATH}"
         sh "echo $PASS | docker login -u $USER --password-stdin 192.168.88.14:8083"
         sh 'docker push 192.168.88.14:8083/java-maven-app:4.0'
     }
