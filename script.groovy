7221f395a42e5c7b612f596a781c592791c8559f
Kiseloff <30468578+Kiseloff@users.noreply.github.com>
2022-05-18 17:26:42 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,6 +6,7 @@ def getSrc(projectPath) {
 
 def buildJar(projectPath) {
     echo "building the app..."
+    sh "mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion} versions:commit"
     sh "mvn package -f ${projectPath}"
 }
 
